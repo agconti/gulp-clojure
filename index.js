450f57f76c83d95dcb602a9824efcbfa5cd04417
agconti <agc11d@gmail.com>
2016-01-11 18:45:17 -0500

    fix(index): extracted transfrom function from through2

--- index.js
+++ index.js
@@ -7,39 +7,34 @@ const cljs = require('clojurescript')
 const GulpClojureError = require('./GulpClojureError')
 
 
-module.exports = () => {
-	return through.obj((file, enc, callback) => {
-		if (file.isNull()) {
-			return callback(null, file)
+function transform (file, enc, callback) {
+	if (file.isNull()) { return callback(null, file) }
+
+	if (file.isStream()) {
+		let err = new GulpClojureError('Streaming not supported')
+
+		this.emit('error', err)
+		return callback()
+	}
+
+	try {
+		let compiledJS = cljs.compile(file.contents.toString('utf-8'))
+
+		file.contents = new Buffer(compiledJS)
+
+	} catch (err) {
+		let msg = Array.isArray(err) ? err.join('\n') : err
+		let errorInfo = {
+			fileName: file.path
+		, showStack: false
 		}
 
-		if (file.isStream()) {
-			// let err = new GulpClojureError('Streaming not supported')
-			let err = new gutil.PluginError('gulp-clojure', 'Streaming not supported')
-			return callback(err)
-		}
+		let err = new GulpClojureError(msg, errorInfo)
+		this.emit('error', err)
+	}
 
-		try {
-			let compiledJS = cljs.compile(file.contents.toString())
-
-			if (compiledJS) {
-				file.contents = new Buffer(compiledJS)
-			}
-
-			this.push(file)
-
-		} catch (err) {
-			console.log(`err? ${err}`)
-			let msg = Array.isArray(err) ? err.join('\n') : err
-			let errorInfo = {
-				fileName: file.path
-			, showStack: false
-			}
-			// let err = new GulpClojureError(msg, errorInfo)
-			let err = new gutil.PluginError('gulp-clojure', msg, errorInfo)
-			this.emit('error', err)
-		}
-
-		callback()
-	})
+	this.push(file)
+	callback()
 }
+
+module.exports = () => through.obj(transform)
